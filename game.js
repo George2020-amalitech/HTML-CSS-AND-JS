702e4b0990c6f083f736cd400544fd949619a69d
George Mintah <george.mintah@amalitech.org>
2020-10-11 21:57:25 +0100

    feat: add code to count questions answer and give makes

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choices);
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion ={};
 let acceptingAnswers = true;
@@ -55,7 +56,7 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
-  console.log(availableQuestions);
+
   getNewQuestion();
 };
 
@@ -64,7 +65,9 @@ if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
    return window.location.assign("/end.html");
 }
 
-  questionCounter++;
+
+questionCounter++;
+questionCounterText.innerText =questionCounter + "/" + MAX_QUESTIONS;
 const questionIndex =  Math.floor(Math.random() * availableQuestions.length);
 currentQuestion = availableQuestions[questionIndex];
 question.innerText= currentQuestion.question;
@@ -85,10 +88,24 @@ choices.forEach(choice => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
-    console.log(selectedAnswer === currentQuestion.answer);
+  const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :'incorrect';
+
+if(classToApply ==="correct"){
+  incrementScore(CORRECT_BONUS);
+}
+selectedChoice.parentElement.classList.add(classToApply);
+  setTimeout(() => {
+    selectedChoice.parentElement.classList.remove(classToApply);
     getNewQuestion();
+  }, 1000);
+
 
   });
 });
+
+incrementScore = num => {
+  score +=num;
+  scoreText.innerText = score;
+};
 
 startGame();
