bf3205f8f2cbc0eec45a7ea8e1fb7af3f50fa457
George Mintah <george.mintah@amalitech.org>
2020-10-11 23:06:14 +0100

    feat: add code to show progress made with javascript

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
-
+const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion ={};
 let acceptingAnswers = true;
 let score = 0;
@@ -68,6 +68,8 @@ if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
 
 questionCounter++;
 progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)*100}%`;
+
 const questionIndex =  Math.floor(Math.random() * availableQuestions.length);
 currentQuestion = availableQuestions[questionIndex];
 question.innerText= currentQuestion.question;
