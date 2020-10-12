5d30e2ea7e3540f206bc75e72005651adcaa9437
George Mintah <george.mintah@amalitech.org>
2020-10-12 01:40:47 +0100

    feat: add code to load question from online

--- game.js
+++ game.js
@@ -9,44 +9,37 @@ let score = 0;
 let questionCounter= 0;
 let availableQuestions =[];
 
-let questions = [
-  {
-    question: "The development environment offers which standard construct for data validation ",
-    choice1: "Super controlled loop constructs",
-    choice2: "Case sensitivity check",
-    choice3:"Validation constructs",
-    choice4:"All of the mentioned",
-    answer: 4
-  },{
-    question: "The main purpose of a Live Wire in NetScape is to ",
-    choice1: "Create linkage between client side and server side",
-    choice2: "Permit server side, JavaScript code, to connect to RDBMS",
-    choice3:"Support only non relational database",
-    choice4:"To interpret JavaScript code",
-    answer: 2
-  },{
-    question: "The script tag must be placed in ",
-    choice1: "head",
-    choice2: "head and body",
-    choice3:"title and head",
-    choice4:"All of the mentioned",
-    answer: 2
-  },{
-    question: "A JavaScript program developed on a Unix Machine ",
-    choice1: "will throw errors and exceptions",
-    choice2: "must be restricted to a Unix Machine only",
-    choice3:"will work perfectly well on a Windows Machine",
-    choice4:"will be displayed as a JavaScript text on the browser",
-    answer: 3
-  },{
-    question: "JavaScript is ideal to ",
-    choice1: "make computations in HTML simpler",
-    choice2: "minimize storage requirements on the web server",
-    choice3:"increase the download time for the client",
-    choice4:"none of the mentioned",
-    answer: 4
-  }
-];
+let questions = [];
+
+fetch(
+    "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
+)
+    .then((res) => {
+    return res.json();
+    })
+    .then((loadedQuestions) => {
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+    const formattedQuestion = {
+    question: loadedQuestion.question,
+    };
+
+    const answerChoices = [...loadedQuestion.incorrect_answers];
+    formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+   answerChoices.splice(
+    formattedQuestion.answer - 1, 0,
+      loadedQuestion.correct_answer
+  );
+    answerChoices.forEach((choice, index) => {
+    formattedQuestion['choice' + (index + 1)] = choice;
+});
+  return formattedQuestion;
+        });
+        startGame();
+    })
+  .catch((err) => {
+  console.error(err);
+    });
+
 
 //constants
 const CORRECT_BONUS = 20;
@@ -110,5 +103,3 @@ incrementScore = num => {
   score +=num;
   scoreText.innerText = score;
 };
-
-startGame();
